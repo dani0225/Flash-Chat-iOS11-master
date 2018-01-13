afbf6ce0db16860c4596ef5760a4147d9e974af3
Admin <admin@admins-macbook-pro-2.local>
2018-01-13 16:16:26 +0200

    am creat classless messages is user

---
+++ Flash Chat/Messages.swift
@@ -0,0 +1,17 @@
+//
+//  Messages.swift
+//  Flash Chat
+//
+//  Created by Admin on 13/01/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Messages: NSManagedObject {
+
+    
+    
+    
+}
---
+++ Flash Chat/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Flash Chat
+//
+//  Created by Admin on 13/01/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class User: NSManagedObject {
+
+}
